a3be32ab62f7ed35aa0e5e4d68e2320d18a7b25c
bombpersons <blindrabbits@gmail.com>
2016-02-14 01:04:48 +0000

    Asteroids no longer look like they teleport when they get to the edge of the screen.

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,14 @@
 module Main where
 import           Control.Monad
-import qualified Data.Map               as Map
+import qualified Data.List              as List
+import           Data.Maybe
 import           SFML.Graphics
 import           SFML.Window
 import           System.Random
 
 import           Paths_HaskellAsteroids
 
+-- Utility functions
 multVec :: Vec2f -> Float -> Vec2f
 multVec (Vec2f x y) n = Vec2f (x * n) (y * n)
 
@@ -22,6 +24,17 @@ vecLength (Vec2f x y) = sqrt ((x * x) + (y * y))
 vecNormalise :: Vec2f -> Vec2f
 vecNormalise v = divVec v (vecLength v)
 
+-- Input
+worldIsKeyDown :: World -> KeyCode -> Bool
+worldIsKeyDown w@World{wCurKeysDown=keys} key = isJust (List.find (==key) keys)
+
+worldIsKeyPressed :: World -> KeyCode -> Bool
+worldIsKeyPressed w@World{wCurKeysDown=curKeys, wOldKeysDown=oldKeys} key = isJust (List.find (==key) curKeys) && isNothing (List.find (==key) oldKeys)
+
+worldIsKeyReleased :: World -> KeyCode -> Bool
+worldIsKeyReleased w@World{wCurKeysDown=curKeys, wOldKeysDown=oldKeys} key = isNothing (List.find (==key) curKeys) && isJust (List.find (==key) oldKeys)
+
+-- Entity system
 data Entity = Entity {
   eSprite        :: Maybe Sprite,
   ePosition      :: Vec2f,
@@ -31,6 +44,12 @@ data Entity = Entity {
   eRender        :: World -> IO (),
   eUpdate        :: World -> Float -> Entity
 }
+
+data World = World { wWindow      :: RenderWindow,
+                     wOldKeysDown :: [KeyCode],
+                     wCurKeysDown :: [KeyCode],
+                     wWorldRect   :: FloatRect,
+                     wEntities    :: [Entity] }
 
 emptyEntity :: Entity
 emptyEntity = Entity {
@@ -44,29 +63,30 @@ emptyEntity = Entity {
 }
 
 drawSpriteAtPos :: Entity -> World -> IO ()
-drawSpriteAtPos e world@World{wWindow=wnd} =
+drawSpriteAtPos e world@World{wWindow=wnd, wWorldRect=FloatRect wx wy ww wh} =
   case eSprite e of Nothing -> return ()
                     Just spr -> do
                       texRect <- getTextureRect spr
                       let width = fromIntegral (iwidth texRect)
                           height = fromIntegral (iheight texRect)
+                      let pos = (ePosition e) + (divVec (Vec2f wx wy) 2)
 
                       setOrigin spr (Vec2f (width / 2) (height / 2))
-                      setPosition spr (ePosition e)
+                      setPosition spr pos
                       setRotation spr (eRotation e)
                       drawSprite wnd spr Nothing
 
 moveAtVelocity :: Entity -> World -> Float -> Entity
-moveAtVelocity e@Entity{eVelocity=Vec2f vx vy, ePosition=Vec2f px py, eRotation=r, eRotationSpeed=rs} w@World{wWindowSize=Vec2f wx wy} dt =
-  let possiblePx = px + vx * dt
-      possiblePy = py + vy * dt
+moveAtVelocity e@Entity{eVelocity=Vec2f vx vy, ePosition=Vec2f px py, eRotation=r, eRotationSpeed=rs} w@World{wWorldRect=FloatRect wx wy ww wh} dt =
+  let possiblePx = (px + vx * dt)
+      possiblePy = (py + vy * dt)
       newPx
-        | possiblePx > wx = possiblePx - wx
-        | possiblePx < 0 = wx - possiblePx
+        | possiblePx > ww = possiblePx - ww
+        | possiblePx < 0 = ww - possiblePx
         | otherwise = possiblePx
       newPy
-        | possiblePy > wy = possiblePy - wy
-        | possiblePy < 0 = wy - possiblePy
+        | possiblePy > wh = possiblePy - wh
+        | possiblePy < 0 = wh - possiblePy
         | otherwise = possiblePy
   in e { ePosition = Vec2f newPx newPy, eRotation = r + rs * dt}
 
@@ -75,10 +95,6 @@ asteroid ent = ent { eRender = drawSpriteAtPos ent,
 
 player ent = ent { eRender = drawSpriteAtPos ent,
                    eUpdate = \world delta -> player (moveAtVelocity ent world delta) }
-
-data World = World { wWindow     :: RenderWindow,
-                     wWindowSize :: Vec2f,
-                     wEntities   :: [Entity] }
 
 updateWorld :: World -> Float -> World
 updateWorld world@World{wEntities=ent} dt = world {wEntities = fmap update ent }
@@ -97,7 +113,7 @@ makeAsteroid t = do
   gen <- newStdGen
   let (vx, gen1) = randomR (-1, 1) gen :: (Float, StdGen)
       (vy, gen2) = randomR (-1, 1) gen1 :: (Float, StdGen)
-      dir = (Vec2f vx vy)
+      dir = vecNormalise (Vec2f vx vy)
 
   let (speed, gen3) = randomR (1, 100) gen2 :: (Float, StdGen)
       (rotSpeed, gen4) = randomR (-100, 100) gen3 :: (Float, StdGen)
@@ -108,6 +124,8 @@ makeWorld :: RenderWindow -> Texture -> Texture -> IO World
 makeWorld wnd playerTex asteroidTex = do
   -- Make a bunch of asteroids.
   asteroids <- mapM (\dummy -> makeAsteroid asteroidTex) [1..10]
+  asteroidSize <- textureSize asteroidTex
+  let (Vec2f worldExtend _) = toVec2f asteroidSize
 
   -- Make a single player.
   playerSprite <- err createSprite
@@ -117,16 +135,22 @@ makeWorld wnd playerTex asteroidTex = do
   -- Create the world from these.
   let entities = playerEnt : asteroids
   windowSizeU <- getWindowSize wnd
-  let windowSize = toVec2f windowSizeU
+  let Vec2f x y = toVec2f windowSizeU
+      offset = worldExtend * 1.5
+      worldRect = FloatRect (0 - offset) (0 - offset) (x + offset) (y + offset)
 
-  let world = World { wWindow = wnd, wWindowSize = windowSize, wEntities = entities }
+  let world = World { wWindow = wnd,
+                      wOldKeysDown = [],
+                      wCurKeysDown = [],
+                      wWorldRect = worldRect,
+                      wEntities = entities }
   return world
 
 main :: IO ()
 main = do
   -- Make a window.
   let ctxSettings = Just $ ContextSettings 24 8 0 1 2 [ContextDefault]
-  wnd <- createRenderWindow (VideoMode 640 480 32) "Haskell Asteroids" [SFDefaultStyle] ctxSettings
+  wnd <- createRenderWindow (VideoMode 800 600 32) "Hasteroids" [SFDefaultStyle] ctxSettings
 
   -- Load textures
   asteroidPath <- getDataFileName "data/asteroid.gif"
