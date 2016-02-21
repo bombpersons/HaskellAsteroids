5ccba0bf5e1438fe18e8216bd9e4b617b0b20b2b
bombpersons <blindrabbits@gmail.com>
2016-02-21 21:52:14 +0000

    Converts a lot of stuff to use lens

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,7 @@
 module Main where
+import           Control.Lens
 import           Control.Monad
+import           Control.Monad.State
 import qualified Data.List              as List
 import qualified Data.Map               as Map
 import           Data.Maybe
@@ -12,54 +14,8 @@ import           World
 
 import           Paths_HaskellAsteroids
 
-makeAsteroid :: Texture -> IO Entity
-makeAsteroid t = do
-  -- Make it go in a random direction.
-  gen <- newStdGen
-  let (vx, gen1) = randomR (-1, 1) gen :: (Float, StdGen)
-      (vy, gen2) = randomR (-1, 1) gen1 :: (Float, StdGen)
-      dir = vecNormalise (Vec2f vx vy)
-
-  let (speed, gen3) = randomR (1, 100) gen2 :: (Float, StdGen)
-      (rotSpeed, gen4) = randomR (-pi, pi) gen3 :: (Float, StdGen)
-
-  return $ emptyEntity {eVelocity = multVec dir speed, eRotationSpeed = rotSpeed, eTexture = Just t}
-
 makeWorld :: RenderWindow -> IO World
-makeWorld wnd = do
-  -- Load textures
-  asteroidPath <- getDataFileName "data/asteroid.png"
-  playerPath <- getDataFileName "data/player.png"
-  bulletPath <- getDataFileName "data/bullet.png"
-  asteroidTex <- err $ textureFromFile asteroidPath Nothing
-  playerTex <- err $ textureFromFile playerPath Nothing
-  bulletTex <- err $ textureFromFile bulletPath Nothing
-
-  -- Make a bunch of asteroids.
-  asteroids <- mapM (\dummy -> makeAsteroid asteroidTex) [1..10]
-  asteroidSize <- textureSize asteroidTex
-  let (Vec2f worldExtend _) = toVec2f asteroidSize
-
-  -- Make a single player.
-  let playerEnt = emptyEntity { ePosition = Vec2f 200 200, eRotation = pi/2, eTexture = Just playerTex }
-
-  -- Create the world from these.
-  let entities = playerEnt : asteroids
-  windowSizeU <- getWindowSize wnd
-  let Vec2f x y = toVec2f windowSizeU
-      offset = worldExtend * 1.5
-      worldRect = FloatRect (0 - offset) (0 - offset) (x + offset) (y + offset)
-
-  let world = World { wWindow = wnd,
-                      wOldKeysDown = [],
-                      wCurKeysDown = [],
-                      wWorldRect = worldRect,
-                      wBulletTexture = bulletTex,
-                      wPlayer = playerEnt,
-                      wPlayerShootTimer = 0,
-                      wAsteroids = asteroids,
-                      wBullets = [] }
-  return world
+makeWorld wnd = return $ window .~ Just wnd $ defWorld
 
 main :: IO ()
 main = do
@@ -79,19 +35,23 @@ main = do
   destroy wnd
 
 processEvents :: World -> IO (Maybe World)
-processEvents w@World{wWindow=wnd, wCurKeysDown=curKeys} = do
-  evt <- pollEvent wnd
-  case evt of
-    Just SFEvtClosed -> return Nothing
-    Just (SFEvtKeyPressed c _ _ _ _) -> processEvents w {wCurKeysDown = c : curKeys}
-    Just (SFEvtKeyReleased c _ _ _ _) -> processEvents w {wCurKeysDown = List.delete c curKeys}
+processEvents w =
+  case w ^. window of
+    Just wnd -> do
+      evt <- pollEvent wnd
+      case evt of
+        Just SFEvtClosed -> return Nothing
+        Just (SFEvtKeyPressed c _ _ _ _) -> processEvents $ curKeysDown %~ (c:) $ w
+        Just (SFEvtKeyReleased c _ _ _ _) -> processEvents $ curKeysDown %~ List.delete c $ w
+        Nothing -> return (Just w)
+        _ -> processEvents w
     Nothing -> return (Just w)
-    _ -> processEvents w
 
 loop :: Clock -> World -> IO ()
-loop clock world@World{wWindow=wnd}= do
+loop clock world@World{}= do
   -- Copy over the current keys to the old keys List
-  let keyUpdatedWorld = world {wOldKeysDown = wCurKeysDown world}
+  let keysDown = world ^. curKeysDown
+  let keyUpdatedWorld = set oldKeysDown keysDown world
 
   -- Poll for events
   polledWorld <- processEvents keyUpdatedWorld
@@ -99,6 +59,7 @@ loop clock world@World{wWindow=wnd}= do
     Nothing -> return ()
     Just validWorld -> do
       -- Clear the window
+      let Just wnd = world^.window
       clearRenderWindow wnd $ Color 0 0 0 255
 
       -- Get delta time
@@ -106,7 +67,7 @@ loop clock world@World{wWindow=wnd}= do
       let dt = asSeconds dtMicro
 
       -- Update world
-      let updatedWorld = updateWorld validWorld dt
+      let updatedWorld = execState (updateWorld dt) validWorld
       renderWorld updatedWorld
 
       -- Show the window.
--- src/World.hs
+++ src/World.hs
@@ -1,149 +1,133 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module World where
-import qualified Data.List     as List
-import qualified Data.Map      as Map
+import           Control.Lens
+import           Control.Monad.State
+import qualified Data.List           as List
+import qualified Data.Map            as Map
 import           Data.Maybe
 import           SFML.Graphics
 import           SFML.Window
-
 import           Utils
 
--- Entity system
-data Entity = Entity {
-  eTexture       :: Maybe Texture,
-  ePosition      :: Vec2f,
-  eRotation      :: Float,
-  eVelocity      :: Vec2f,
-  eRotationSpeed :: Float
+type Entity = Int
+
+data Transformable = Transformable {
+  _position :: Vec2f,
+  _rotation :: Float
+}
+makeLenses ''Transformable
+
+data Physics = Physics {
+  _velocity        :: Vec2f,
+  _rotationalSpeed :: Float
+}
+makeLenses ''Physics
+
+type TextureID = Int
+
+data Graphics = Graphics {
+  _textureID :: TextureID
+}
+makeLenses ''Graphics
+
+type ComponentMap = Map.Map Entity
+
+componentIntersection2 :: ComponentMap a -> ComponentMap b -> ComponentMap (a, b)
+componentIntersection2 = Map.intersectionWith (\x y -> (x, y))
+
+componentIntersection3 :: ComponentMap a -> ComponentMap b -> ComponentMap c -> ComponentMap (a, b, c)
+componentIntersection3 a b = Map.intersectionWith (\(x, y) z -> (x, y, z)) (componentIntersection2 a b)
+
+componentIntersection4 :: ComponentMap a -> ComponentMap b -> ComponentMap c -> ComponentMap d -> ComponentMap (a, b, c, d)
+componentIntersection4 a b c = Map.intersectionWith (\(x, y, z) w -> (x, y, z, w)) (componentIntersection3 a b c)
+
+data World = World {
+  _window         :: Maybe RenderWindow,
+  _oldKeysDown    :: [KeyCode],
+  _curKeysDown    :: [KeyCode],
+
+  _textures       :: [Texture],
+
+  _lastID         :: Entity,
+
+  _transformables :: ComponentMap Transformable,
+  _physics        :: ComponentMap Physics,
+  _graphics       :: ComponentMap Graphics
+}
+makeLenses ''World
+
+type WorldState = State World
+
+defWorld :: World
+defWorld = World {
+  _window = Nothing,
+  _oldKeysDown = [],
+  _curKeysDown = [],
+
+  _textures = [],
+
+  _lastID = 0,
+
+  _transformables = Map.empty,
+  _physics = Map.empty,
+  _graphics = Map.empty
 }
 
-emptyEntity :: Entity
-emptyEntity = Entity {
-                      eTexture = Nothing,
-                      ePosition = Vec2f 0 0,
-                      eRotation = 0,
-                      eVelocity = Vec2f 0 0,
-                      eRotationSpeed = 0
-}
+-- Adding entities / components
+newEntity :: WorldState Entity
+newEntity = lastID <+= 1
 
-data World = World { wWindow           :: RenderWindow,
-                     wOldKeysDown      :: [KeyCode],
-                     wCurKeysDown      :: [KeyCode],
-                     wWorldRect        :: FloatRect,
+addComponent :: Entity -> c -> ASetter' World (ComponentMap c) -> WorldState ()
+addComponent e c l = l %= Map.insert e c
 
-                     wBulletTexture    :: Texture,
+removeComponent :: Entity -> ASetter' World (ComponentMap c) -> WorldState ()
+removeComponent e l = l %= Map.delete e
 
-                     wPlayer           :: Entity,
-                     wPlayerShootTimer :: Float,
-
-                     wAsteroids        :: [Entity],
-                     wBullets          :: [Entity]
-}
+-- Textures
+getWorldTexture :: World -> TextureID -> Maybe Texture
+getWorldTexture World{_textures=textures} texID
+  | texID >= texLength = Nothing
+  | texID < 0 = Nothing
+  | otherwise = Just $ textures !! texID
+  where texLength = length textures
 
 -- Input
 worldIsKeyDown :: World -> KeyCode -> Bool
-worldIsKeyDown w@World{wCurKeysDown=keys} key = isJust (List.find (==key) keys)
+worldIsKeyDown w@World{_curKeysDown=keys} key = isJust (List.find (==key) keys)
 
 worldIsKeyPressed :: World -> KeyCode -> Bool
-worldIsKeyPressed w@World{wCurKeysDown=curKeys, wOldKeysDown=oldKeys} key = isJust (List.find (==key) curKeys) && isNothing (List.find (==key) oldKeys)
+worldIsKeyPressed w@World{_curKeysDown=curKeys, _oldKeysDown=oldKeys} key = isJust (List.find (==key) curKeys) && isNothing (List.find (==key) oldKeys)
 
 worldIsKeyReleased :: World -> KeyCode -> Bool
-worldIsKeyReleased w@World{wCurKeysDown=curKeys, wOldKeysDown=oldKeys} key = isNothing (List.find (==key) curKeys) && isJust (List.find (==key) oldKeys)
+worldIsKeyReleased w@World{_curKeysDown=curKeys, _oldKeysDown=oldKeys} key = isNothing (List.find (==key) curKeys) && isJust (List.find (==key) oldKeys)
 
 -- Update
-moveAtVelocity :: World -> Float -> Entity -> Entity
-moveAtVelocity w@World{wWorldRect=FloatRect wx wy ww wh} dt e@Entity{eVelocity=Vec2f vx vy, ePosition=Vec2f px py, eRotation=r, eRotationSpeed=rs} =
-  let possiblePx = (px + vx * dt)
-      possiblePy = (py + vy * dt)
-      newPx
-        | possiblePx > ww = possiblePx - ww
-        | possiblePx < 0 = ww - possiblePx
-        | otherwise = possiblePx
-      newPy
-        | possiblePy > wh = possiblePy - wh
-        | possiblePy < 0 = wh - possiblePy
-        | otherwise = possiblePy
-  in e{ePosition = Vec2f newPx newPy, eRotation = r + rs * dt}
+updateWorld :: Float -> WorldState ()
+updateWorld dt = do
+  return ()
 
-moveBackAndForwardKeys :: Float -> World -> Float -> Entity -> Entity
-moveBackAndForwardKeys acc w dt e@Entity{eVelocity=v, eRotation=r} = e{eVelocity = newVel}
-  where up = if worldIsKeyDown w KeyUp then (-1) else 0
-        down = if worldIsKeyDown w KeyDown then 1 else 0
-        dir = multVec (getForwardVecFromRotation r) (up + down)
-        newVel = v + multVec dir (acc * dt)
+-- Render
+drawTexture :: Maybe RenderWindow -> Maybe Texture -> Vec2f -> Float -> IO ()
+drawTexture (Just wnd) (Just t) pos rot = do
+  spr <- err createSprite
+  setTexture spr t True
+  texRect <- getTextureRect spr
+  let width = fromIntegral (iwidth texRect)
+      height = fromIntegral (iheight texRect)
 
-turnLeftRightKeys :: Float -> World -> Float -> Entity -> Entity
-turnLeftRightKeys acc w dt e@Entity{eRotationSpeed=v} = e{eRotationSpeed = newSpeed}
-  where left = if worldIsKeyDown w KeyLeft then (-1) else 0
-        right = if worldIsKeyDown w KeyRight then 1 else 0
-        dir = left + right
-        newSpeed = v + dir * acc * dt
+  setOrigin spr (Vec2f (width / 2) (height / 2))
+  setPosition spr pos
+  setRotation spr (toDegrees rot)
+  drawSprite wnd spr Nothing
+drawTexture _ _ _ _ = return ()
 
-updateEntity :: World -> Float -> Entity -> Entity
-updateEntity = moveAtVelocity
-
-updatePlayer :: Float -> World -> World
-updatePlayer dt w@World{wPlayer=player} = w{wPlayer=newPlayer}
-  where newPlayer = moveAtVelocity w dt .
-                    moveBackAndForwardKeys 500 w dt .
-                    turnLeftRightKeys (pi*2) w dt $ player
-
-newBullet :: World -> Vec2f -> Float -> Entity
-newBullet World{wBulletTexture=bulletTex} pos rot = emptyEntity{eTexture=Just bulletTex,
-                                                                ePosition=pos,
-                                                                eRotation=rot,
-                                                                eVelocity= flip multVec (-200) . getForwardVecFromRotation $ rot}
-
-playerShoot :: Float -> World -> World
-playerShoot dt w@World{wBullets=bullets, wPlayerShootTimer=timer, wPlayer=p@Entity{ePosition=pPos, eRotation=pRot}}
-  | timer > 0 = w{wPlayerShootTimer=timer - dt}
-  | worldIsKeyPressed w KeySpace = w{wBullets=newBullet w pPos pRot : bullets, wPlayerShootTimer = 1}
-  | otherwise = w
-
-updateAsteroid :: World -> Float -> Entity -> Entity
-updateAsteroid = moveAtVelocity
-
-updateAsteroids :: Float -> World -> World
-updateAsteroids dt w@World{wAsteroids=asteroids} = w{wAsteroids=newAsteroids}
-  where newAsteroids = fmap (updateAsteroid w dt) asteroids
-
-updateBullet :: World -> Float -> Entity -> Entity
-updateBullet = moveAtVelocity
-
-updateBullets :: Float -> World -> World
-updateBullets dt w@World{wBullets=bullets} = w{wBullets=newBullets}
-  where newBullets = fmap (updateBullet w dt) bullets
-
-collideBullets :: World -> World
-collideBullets world@World{wBullets=bullets} = foldr collideBullet world bullets
-  where collideBullet e world = world
-
-updateWorld :: World -> Float -> World
-updateWorld w dt = foldl foldFunc w updateFuncs
-  where
-    foldFunc world f = f world
-    updateFuncs = [updatePlayer dt,
-                   playerShoot dt,
-                   updateAsteroids dt,
-                   updateBullets dt]
-
-drawSpriteAtPos :: World -> Entity -> IO ()
-drawSpriteAtPos world@World{wWindow=wnd, wWorldRect=FloatRect wx wy ww wh} e@Entity{eTexture=t} =
-  case t of Nothing -> return ()
-            Just tex -> do
-              spr <- err createSprite
-              setTexture spr tex True
-              texRect <- getTextureRect spr
-              let width = fromIntegral (iwidth texRect)
-                  height = fromIntegral (iheight texRect)
-              let pos = ePosition e + divVec (Vec2f wx wy) 2
-
-              setOrigin spr (Vec2f (width / 2) (height / 2))
-              setPosition spr pos
-              setRotation spr (toDegrees (eRotation e))
-              drawSprite wnd spr Nothing
+renderEntity :: World -> (Graphics, Transformable) -> IO ()
+renderEntity w@World{_window=wnd}
+            (Graphics{_textureID=gid}, Transformable{_position=pos, _rotation=rot}) = drawTexture wnd tex pos rot
+  where tex = getWorldTexture w gid
 
 renderWorld :: World -> IO ()
-renderWorld world@World{wPlayer=player, wAsteroids=asteroids, wBullets=bullets} = do
-  mapM_ (drawSpriteAtPos world) asteroids
-  mapM_ (drawSpriteAtPos world) bullets
-  drawSpriteAtPos world player
+renderWorld w@World{_graphics=graphics, _transformables=transforms} = do
+  let graphicsAndTransforms = componentIntersection2 graphics transforms
+  mapM_ (renderEntity w) graphicsAndTransforms
